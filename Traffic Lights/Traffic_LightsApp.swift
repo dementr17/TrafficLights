fd1eb9760ca409b85186b7ba9206d358979afb9f
Dmitry <dementr17@yandex.ru>
2022-02-15 13:46:42 +0300

    first commit

---
+++ Traffic Lights/Traffic_LightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Traffic_LightsApp.swift
+//  Traffic Lights
+//
+//  Created by Дмитрий Чепанов on 15.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct Traffic_LightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
