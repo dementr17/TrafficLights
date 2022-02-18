85d9a71a911d84ca3ef41326ec00e6ddf31ec585
Dmitry <dementr17@yandex.ru>
2022-02-18 17:19:05 +0300

    2

--- Traffic Lights/ButtonView.swift
+++ Traffic Lights/ButtonView.swift
@@ -15,17 +15,17 @@ struct ButtonView: View {
     var body: some View {
         Button(action: action) {
             Text(title)
-                    .font(.title)
-                    .foregroundColor(.white)
-                    .bold()
-                    .frame(width: 150, height: 60)
-                    .background(Color.blue)
-                    .cornerRadius(15)
-                    .overlay(
-                                RoundedRectangle(cornerRadius: 20)
-                                    .stroke(Color.white, lineWidth: 5)
-                            )
-            }
+                .font(.title)
+                .foregroundColor(.white)
+                .bold()
+                .frame(width: 150, height: 60)
+                .background(Color.blue)
+                .cornerRadius(15)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 20)
+                        .stroke(Color.white, lineWidth: 5)
+                )
+        }
     }
 }
 
